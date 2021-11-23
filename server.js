a53a4d477c8bd6e1070f3fee1ae8768d12a67188
Jimmy <odylic.lin@gmail.com>
2021-11-23 14:37:07 -0800

    server

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
-const app = require("express")();
-const express = require("express");
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-app.use(express.static(__dirname + "/public"));
-
-io.on("connection", (socket) => {
-  socket.on("message", ({ name, message }) => {
-    io.emit("message", { name, message });
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  transports: ["websocket", "polling"],
+});
+const users = {};
+io.on("connection", (client) => {
+  client.on("username", (username) => {
+    const user = {
+      name: username,
+      id: client.id,
+    };
+  });
+  client.on("send", (message) => {
+    io.emit("message", {
+      text: message,
+      date: new Date().toISOString(),
+      user: users[client.id],
+    });
+  });
+  client.on("disconnect", () => {
+    const username = users[client.id];
+    delete users[client.id];
+    io.emit("disconnected", client.id);
   });
 });
 
-app.get('/app', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'));
+server.listen(4000);
+
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require("dotenv").config();
+const PORT = 3000;
+app.use(express.static(__dirname + "/public"));
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Hello from test endpoint" });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server this time" });
+});
+
+app.get("/app", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
-http.listen(4000, function () {
-  console.log("listening on Port 4000");
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on ${PORT} 🚀`);
 });
