68648715aff3d4b0dc0476fedf2475ed9e969571
Jimmy <odylic.lin@gmail.com>
2021-11-22 17:44:51 -0800

    webpack

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,68 @@ import "../App.scss";
 const socket = io.connect("http://localhost:4000");
 
 function App() {
-  return <div className="App">From React component</div>;
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on('message', ({ name, message }) => {
+      setChat([...chat, {name, message}])
+    })
+  })
+
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("message", { name, message });
+    setState({ message: "", name });
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}: <span>{message} </span>
+        </h3>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="Card">
+      Test
+      <form onSubmit={onMessageSubmit}>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <TextField
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.name}
+            label="Name"
+          />
+        </div>
+        <div>
+          <TextField
+            name="message"
+            onChange={(e) => onTextChange(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message"
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderchat()}
+      </div>
+    </div>
+  );
 }
 
 export default App;
